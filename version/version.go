bbe9ddad3f6765f5e7e4d29c103d4e265a6ba1d7
Nir Rozenbaum <nirro@il.ibm.com>
2025-07-18 10:49:53 +0300

    Updates artifacts for v0.5.0-rc.3 release
    Signed-off-by: Nir Rozenbaum <nirro@il.ibm.com>

--- version/version.go
+++ version/version.go
@@ -18,5 +18,5 @@ package version
 
 const (
 	// BundleVersion is the value used for labeling the version of the gateway-api-inference-extension.
-	BundleVersion = "v0.5.0-rc.2"
+	BundleVersion = "v0.5.0-rc.3"
 )
